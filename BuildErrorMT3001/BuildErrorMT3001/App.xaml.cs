b10529f932114439d06d2e1437cf1e20ffc160dd
Adrian Hofer <adrian.hofer@hotmail.com>
2020-07-15 09:55:46 +0200

    Add project files.

---
+++ BuildErrorMT3001/BuildErrorMT3001/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using BuildErrorMT3001.Services;
+using BuildErrorMT3001.Views;
+
+namespace BuildErrorMT3001 {
+    public partial class App : Application {
+
+        public App() {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart() {
+        }
+
+        protected override void OnSleep() {
+        }
+
+        protected override void OnResume() {
+        }
+    }
+}
---
+++ BuildErrorMT3001/BuildErrorMT3001/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using BuildErrorMT3001.Models;
+
+namespace BuildErrorMT3001.ViewModels {
+    public class ItemDetailViewModel : BaseViewModel {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null) {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
